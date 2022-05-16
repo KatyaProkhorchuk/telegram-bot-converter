fe48330ba93bb3df74cb58d7c3f12d2084bf94b9
Katya <prokhorchuk.e@phystech.edu>
2022-05-16 22:18:13 +0300

    readme fix and add script

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
-export TOKENBOTREWIEW=5236391877:AAF14iW9Vwb9z4ofEnnUtxaGImDsRbpVNh4
+export TOKENBOTREWIEW=""
 python3 main.py
